ebd67c2338fb59f41bb6be0e749344f7c340b4d1
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2023-12-31 18:27:36 +0900

    [Gold V] Title: 합분해, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,7 +1,8 @@
 import sys
+from math import factorial
 input = sys.stdin.readline
 
-def boj2225():
+def boj2225_dp():
     N, K = map(int,input().strip().split())
 
     dp = [[1]*(K+1) for _ in range (N+1)]
@@ -11,4 +12,13 @@ def boj2225():
             dp[n][k] = dp[n-1][k]+dp[n][k-1]
     print(dp[N][K] % 1000000000)
 
-if __name__ == "__main__": boj2225()
+def boj2225_H():
+    N, K = map(int,input().strip().split())
+
+    def C(n, r): return factorial(n) // (factorial(r)*factorial(n-r))
+
+    def H(n, r): return C(n+r-1,r)
+
+    print(H(N+1,K-1) % 1000000000)
+
+if __name__ == "__main__": boj2225_H()
