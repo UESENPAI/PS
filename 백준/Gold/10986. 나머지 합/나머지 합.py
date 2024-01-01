00869c482d998ef8af86174888c3f9391dc174bf
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-01-01 16:11:29 +0900

    [Gold III] Title: 나머지 합, Time: 488 ms, Memory: 143316 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,18 +1,22 @@
 import sys
+input = sys.stdin.readline
 
-N, M = map(int, sys.stdin.readline().strip().split())
-A = list(map(int, sys.stdin.readline().strip().split()))
-res = [0 for _ in range(M)]
-res[0] = 1
-
-subsum = 0
-for a in range(len(A)):
-    subsum +=A[a]
-    r=subsum%M
-    res[r]+=1
-
-ans = 0
-for r in res:
-    ans+=r*(r-1)//2
+def boj10986():
+    N, M = map(int, input().strip().split())
+    A = list(map(int, input().strip().split()))
+    res = [0 for _ in range(M)]
+    res[0] = 1
     
-print(ans)
+    subsum = 0
+    for a in range(len(A)):
+        subsum += A[a]
+        r = subsum % M
+        res[r] += 1
+    
+    ans = 0
+    for r in res:
+        ans += r * (r - 1) // 2
+    
+    print(ans)
+
+if __name__ == '__main__': boj10986()
