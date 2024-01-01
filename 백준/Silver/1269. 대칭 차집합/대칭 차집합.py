62b8d7b3846b6237a52ec609a0f0bb49019146a4
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-01-01 12:16:39 +0900

    Delete 백준/Silver directory

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-A, B = map(int,sys.stdin.readline().split())
-
-setA = set(map(int,sys.stdin.readline().split()))
-
-setB = set(map(int,sys.stdin.readline().split()))
-
-print(f'{len(set.difference(set.union(setA,setB), set.intersection(setA,setB)))}')
--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline().strip())
-print(N//5 + N//25 + N//125)
