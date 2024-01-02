f290cd252f65d5b92bd460b9cebdb16a05e87c7d
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-01-02 21:59:21 +0900

    [Gold III] Title: 소수의 연속합, Time: 1208 ms, Memory: 73844 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def boj1644():
+    N = int(input().strip())
+
+    def era_seive(N):
+        sieve = [True] * (N+1)
+        for i in range(2, N+1):
+            if sieve[i]:
+                for j in range(2*i, N+1, i): sieve[j] = False
+
+        return [i for i in range(2, N+1) if sieve[i] == True]
+
+    primes = era_seive(N)
+    ans, fptr, bptr = 0, 0, 0
+    while not bptr > len(primes):
+        subsum = sum(primes[fptr:bptr])
+        if subsum == N:
+            ans += 1
+            bptr += 1
+        elif subsum < N: bptr+=1
+        else: fptr+=1
+    print(ans)
+
+if __name__ == '__main__': boj1644()
