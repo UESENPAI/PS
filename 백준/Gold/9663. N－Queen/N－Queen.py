60ff17db52a51e9444fb65929e18be14a1a00225
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-03-09 23:23:24 +0900

    [Gold IV] Title: N-Queen, Time: 22708 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,30 +1,25 @@
 import sys
-N = int(sys.stdin.readline())
-answer = 0
+input = sys.stdin.readline
 
-def dfs(x, trace):  # x는 현재 행
-    global answer
-    if len(trace) == N:
-        answer += 1
-        return
+def boj9663():
+    N = int(input())
+    answer = 0
+    column = [False] * N
+    diag1 = [False] * (2 * N - 1)
+    diag2 = [False] * (2 * N - 1)
+    
+    def dfs(x):
+        nonlocal answer
+        if x == N:
+            answer += 1
+            return
+        for y in range(N):
+            if not (column[y] or diag1[x + y] or diag2[x - y + N - 1]):
+                column[y] = diag1[x + y] = diag2[x - y + N - 1] = True
+                dfs(x + 1)
+                column[y] = diag1[x + y] = diag2[x - y + N - 1] = False
+                
+    dfs(0)
+    print(answer)
 
-    for y in range(N):
-        check = True
-        for row, col in trace:
-            # 같은 열 제외
-            if y == col:
-                check = False
-                break
-            # 대각선 제외
-            if abs(x-row) == abs(y-col):
-                check = False
-                break
-
-        if check:
-            trace.append((x, y))
-            dfs(x+1, trace)
-            trace.pop()
-
-
-dfs(0, [])
-print(answer)
+if __name__ == '__main__': boj9663()
