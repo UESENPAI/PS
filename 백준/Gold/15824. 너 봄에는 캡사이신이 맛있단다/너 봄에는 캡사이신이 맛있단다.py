4d3b35d9c48e7822dba77268c10856991c70b007
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-05-20 23:31:30 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 2444 ms, Memory: 66316 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -5,12 +5,20 @@ def boj15824():
     N = int(input().strip())
     scoville = sorted(tuple(map(int, input().strip().split())))
     MOD = 1000000007
+
+    def pow(a, b):
+        if not b: return 1
+        elif b == 1: return a
+        else:
+            half = pow(a, b>>1)
+            return half*half*a%MOD if b&1 else half*half%MOD
     
     agony = 0
     for i in range(N):
-        agony += scoville[i] * ((1 << i) - (1 << (N-i-1)))
+        #agony += scoville[i] * ((1 << i) - (1 << (N-i-1)))
+        agony += scoville[i] * (pow(2, i) - pow(2, N - i - 1))
         agony %= MOD
     
     print(agony % MOD)
     
-if __name__ == '__main__': boj15824()
+if __name__ == '__main__': boj15824()  
