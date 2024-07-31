0023c1cf6bae92036239d9d252eb70b231dc5c4e
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-07-31 22:01:17 +0900

    [Gold V] Title: 나의 행렬곱셈 답사기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/12796. 나의 행렬곱셈 답사기/나의 행렬곱셈 답사기.py
@@ -0,0 +1,10 @@
+from sys import stdin, stdout
+input = stdin.readline
+
+def boj12796():
+    K = int(input().strip())
+
+    print(3)
+    print(1, 1, 1, K + 1)
+
+if __name__ == '__main__': boj12796()
