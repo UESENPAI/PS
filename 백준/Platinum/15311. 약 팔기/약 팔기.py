f68eace03b8eb9428e4cabd37dd48e4332c0e534
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-10-02 19:56:39 +0900

    [Platinum V] Title: 약 팔기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+def boj15311():
+    N = int(input().strip())
+    print(2000)
+    print(' '.join(['1'] * 1000))
+    print(' '.join(['1000'] * 1000))
+        
+if __name__ == "__main__": boj15311()
