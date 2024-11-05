4063fbe38d0a85312bcb14ef3b2d03be73934410
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-11-05 21:52:33 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 40544 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def boj14445():
+    N=int(input().strip())
+    
+    def draw_stars(N):
+      if N==1: return ['*']
+
+      stars=draw_stars(N//3)
+      L=[]
+
+      for star in stars: L.append(star*3)
+      for star in stars: L.append(star+' '*(N//3)+star)
+      for star in stars: L.append(star*3)
+
+      return L
+    
+    print('\n'.join(draw_stars(N)))
+
+if __name__ == "__main__": boj14445()
