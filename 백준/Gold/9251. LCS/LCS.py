70b99bc008dff6db67fb15c098278551ed4c5705
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2024-11-13 21:55:34 +0900

    [Gold V] Title: LCS, Time: 348 ms, Memory: 56528 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,12 +1,16 @@
 import sys
+input = sys.stdin.readline
 
-S1 = (sys.stdin.readline().strip())
-S2 = (sys.stdin.readline().strip())
-LCS = [[0 for _ in range(len(S2)+1)] for _ in range(len(S1)+1)]
+def boj9251():
+    S1 = (input().strip())
+    S2 = (input().strip())
+    LCS = [[0 for _ in range(len(S2)+1)] for _ in range(len(S1)+1)]
 
-for s1 in range(1, len(S1)+1):
-    for s2 in range(1, len(S2)+1):
-        if S1[s1-1] == S2[s2-1]: LCS[s1][s2] = LCS[s1-1][s2-1]+1
-        else: LCS[s1][s2] = max(LCS[s1 - 1][s2], LCS[s1][s2 - 1]) 
+    for s1 in range(1, len(S1)+1):
+        for s2 in range(1, len(S2)+1):
+            if S1[s1-1] == S2[s2-1]: LCS[s1][s2] = LCS[s1-1][s2-1]+1
+            else: LCS[s1][s2] = max(LCS[s1 - 1][s2], LCS[s1][s2 - 1]) 
 
-print(max(map(max,LCS)))
+    print(max(map(max,LCS)))
+
+if __name__ == "__main__":boj9251()
