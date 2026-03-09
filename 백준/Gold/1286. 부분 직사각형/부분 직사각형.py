a0768f981703fbd396a4e59c9117a7a9c7991905
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2026-03-09 23:02:59 +0900

    [Gold V] Title: 부분 직사각형, Time: 60 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/1286. 부분 직사각형/부분 직사각형.py
+++ 백준/Gold/1286. 부분 직사각형/부분 직사각형.py
@@ -8,11 +8,16 @@ def boj1286():
     grid = [input().strip() for _ in range(N)]
     count = defaultdict(int)
     
-    for i in range(N<<1):
-        for j in range(M<<1):
-            c = grid[i % N][j % M]
-            count[c] += (i+1)*(j+1)*((N<<1)-i)*((M<<1)-j)
-    
+    for i in range(N):
+        for j in range(M):
+            c = grid[i][j]
+            sigma = 0
+            for k in range(2):
+                for l in range(2):
+                    ii, jj = i + k*N, j + l*M
+                    sigma += (ii+1)*(jj+1)*((N<<1) - ii)*((M<<1) - jj)
+            count[c] += sigma
+
     print(*[count[ch] for ch in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"], sep='\n')
 
 if __name__ == '__main__': boj1286()
