0684c96cbac883ccb111e3fd9c3663ca29df0a20
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2026-03-19 21:23:48 +0900

    [Gold V] Title: 정사면체, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1930. 정사면체/정사면체.py
+++ 백준/Gold/1930. 정사면체/정사면체.py
@@ -1,15 +1,23 @@
 from sys import stdin
 input = stdin.readline
 
+perms = [[0,1,2,3], [0,2,3,1], [0,3,1,2],[1,0,3,2], [1,3,2,0], [1,2,0,3],[2,0,1,3], [2,1,3,0], [2,3,0,1],[3,0,2,1], [3,2,1,0], [3,1,0,2]]
+
+def canonical(t):
+    return min(
+        tuple(t[p[i]] for i in range(4))
+        for p in perms
+    )
+
 def boj1930():
     K = int(input().strip())
-    perms = [[0,1,2,3], [0,2,3,1], [0,3,1,2], [1,0,3,2], [1,3,2,0], [1,2,0,3], [2,0,1,3], [2,1,3,0], [2,3,0,1], [3,0,2,1], [3,2,1,0], [3,1,0,2]]
-    results = []
+    res = []
+
     for _ in range(K):
         data = list(map(int, input().split()))
         a, b = data[:4], data[4:]
-        same = any([b[p[0]], b[p[1]], b[p[2]], b[p[3]]] == a for p in perms)
-        results.append('1' if same else '0')
-    print('\n'.join(results))
+        res.append('1' if canonical(a) == canonical(b) else '0')
+
+    print('\n'.join(res))
 
 if __name__ == '__main__': boj1930()
