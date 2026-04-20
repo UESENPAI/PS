7a2963401aeac51162e3eae5395e352b352f96f6
UESENPAI <127266504+UESENPAI@users.noreply.github.com>
2026-04-20 13:23:26 +0900

    [Gold V] Title: Fraction, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/30855. Fraction/Fraction.py
+++ 백준/Gold/30855. Fraction/Fraction.py
@@ -1,12 +1,55 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+from sys import stdin
+input = stdin.readline
+
+def boj30855():
+    from math import gcd
+
+    def norm(a, b):
+        if b < 0: a, b = -a, -b
+        g = gcd(a, b)
+        return a//g, b//g
+
+    def add(x, y):
+        return norm(x[0]*y[1] + x[1]*y[0], x[1]*y[1])
+
+    def div(x, y):
+        if not y[0]: return None
+        return norm(x[0]*y[1], x[1]*y[0])
+
+    n = int(input())
+    tokens = input().split()
+    if len(tokens) != n:
+        print(-1); return
+
+    st = []
+
+    for t in tokens:
+        if t == ')':
+            try:
+                c = st.pop()
+                b = st.pop()
+                a = st.pop()
+                if st.pop() != '(':
+                    print(-1); return
+            except:
+                print(-1); return
+
+            bc = div(b, c)
+            if bc is None:
+                print(-1); return
+            st.append(add(a, bc))
+
+        elif t == '(':
+            st.append(t)
+
+        else:
+            if not t.isdigit():
+                print(-1); return
+            st.append((int(t), 1))
+
+    if len(st) != 1 or not isinstance(st[0], tuple):
+        print(-1); return
+
+    print(*st[0])
+
+if __name__ == "__main__": boj30855()
